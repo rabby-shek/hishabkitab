308acb373f7c75f868f23a5e034408ff84f288b9
rabby-shek <rabbyshek1998@gmail.com>
2023-01-01 17:48:28 +0600

    added

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -2,11 +2,11 @@ import React, { useRef,useState } from 'react';
 import { Navbar } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 import Nav_data from '../test/nav_data';
-import Swiper from 'swiper';
+
 
 
 import './slider.css'
-import { Autoplay, Pagination, Navigation } from "swiper";
+
 
 
 const Slider = () => {
@@ -14,24 +14,7 @@ const Slider = () => {
   
   return (
     <>
- <div class="swiper">
 
-  <div class="swiper-wrapper">
-
-    <div class="swiper-slide">Slide 1</div>
-    <div class="swiper-slide">Slide 2</div>
-    <div class="swiper-slide">Slide 3</div>
-    ...
-  </div>
-
-  <div class="swiper-pagination"></div>
-
-
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
-
-  <div class="swiper-scrollbar"></div>
-</div>
     
  
 
