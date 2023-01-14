b677635d0c6c0bea802798bcc83d013b35aefa94
suvo-khan <mdsuvoshek1@gmail.com>
2023-01-15 01:21:01 +0600

    added

--- src/components/custom_card/Custom_card_data.js
+++ src/components/custom_card/Custom_card_data.js
@@ -3,57 +3,85 @@ export const getdata = () =>
     {
         "sale":"10%",
         "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2017/06/variable-product-example-2.jpg?fit=430%2C491&ssl=1",
-        "productName":"Box",
-        "productPrice":"30",
+        "productName":"Augue adipiscing euismod",
+        "productPrice":"199",
         "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
     },
     {
-        "sale":"10%",
+        "sale":"15%",
         "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/eames-lounge-chair-1.jpg",
-        "productName":"Box",
-        "productPrice":"30",
+        "productName":"Eames lounge chair",
+        "productPrice":"399",
         "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
     },
     {
-        "sale":"10%",
+        "sale":"5%",
         "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/eames-plastic-side-chair-3.jpg",
-        "productName":"Box",
+        "productName":"Eames plastic side chair",
+        "productPrice":"99",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"16%",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2021/07/senectus-tincidunt-2.jpg",
+        "productName":"Henectus tincidunt",
         "productPrice":"30",
         "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
     },
     {
-        "sale":"10%",
+        "sale":"New",
         "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
-        "productName":"Box",
-        "productPrice":"30",
+        "productName":"Ornare auctor",
+        "productPrice":"40",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"23%",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/panton-tunior-chair-2.jpg",
+        "productName":"Panton tunior chair",
+        "productPrice":"36",
         "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
     },
     {
         "sale":"10%",
-        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
-        "productName":"Box",
-        "productPrice":"30",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/smart-watches-wood-edition-1.jpg",
+        "productName":"Smart watches wood edition",
+        "productPrice":"599",
         "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
     },
     {
         "sale":"10%",
-        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
-        "productName":"Box",
-        "productPrice":"30",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/wine-bottle-lantern-1.jpg",
+        "productName":"Wine bottle lantern",
+        "productPrice":"399",
         "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
     },
     {
-        "sale":"10%",
-        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
-        "productName":"Box",
-        "productPrice":"30",
+        "sale":"13%",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/wooden-single-drawer-1.jpg",
+        "productName":"Wooden single drawer",
+        "productPrice":"99",
         "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
     },
     {
-        "sale":"10%",
-        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
-        "productName":"Box",
-        "productPrice":"30",
+        "sale":"48%",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/decoration-wooden-presen-3.jpg",
+        "productName":"Louis Poulsen",
+        "productPrice":"50",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"28%",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2021/07/classic-wooden-chair-1.jpg",
+        "productName":"Classic wooden chair",
+        "productPrice":"50",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"13%",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/iphone-dock-1.jpg",
+        "productName":"iPhone dock",
+        "productPrice":"50",
         "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
     }
   
