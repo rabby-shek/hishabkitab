69797ab5779ae8c311fda9da427db389d767dc67
suvo-khan <mdsuvoshek1@gmail.com>
2023-01-15 00:34:45 +0600

    added

--- src/components/custom_card/Card.js
+++ src/components/custom_card/Card.js
@@ -1,30 +1,84 @@
 import React from 'react';
 import './card.css'
 import { MdCompareArrows } from 'react-icons/md';
+import { AiFillHeart } from 'react-icons/ai';
+import { AiFillEye } from 'react-icons/ai';
+import { FaLongArrowAltDown } from 'react-icons/fa';
+import { BsFillCartFill } from 'react-icons/bs';
+import { getdata } from './Custom_card_data'
 
-const Card = () => {
+
+const Card = (props) => {
+
+    let Data = getdata();
+
+
     return (
-        <div className='custom_card'>
-            <div className='custom_card_box'>
-                <div className='card_image'>
-                    <img src='https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1' />
+        <div className='container custom_card'>
+         <div className='row'>
+                <h2>Featured products</h2>
 
-                </div>
-                <div>
-                    compare
-                    </div>
+            </div>
+            <div className='row'>
+           {
+                Data.map(card_info => {
+                    return (
+                        
 
 
-
-                <h3>card title</h3>
-                <div className="custom_card_body">
-                    the name of our country is bangladesh. there are many problem in our country.
-                    wee need try to solve them.
-
-                </div>
+<div className='col-lg-3 col-md-4 mb-3'>
+    <div className='product-box'>
+        <div className='product-inner-box position-relative'>
+            <div className='icons position-absolute'>
+                <a href="#" className='text-decoration-none text-dark'><AiFillHeart /></a>
+                <a href="#" className='text-decoration-none text-dark'><MdCompareArrows /></a>
+                <a href="#" className='text-decoration-none text-dark'><AiFillEye /></a>
 
 
             </div>
+            <div className='onsale'>
+                <span className='badge rounded-0'><FaLongArrowAltDown />{card_info.sale}</span>
+
+            </div>
+            <img src={card_info.photo} alt="photo" className='img-fluid' />
+            <div className='cart-btn'>
+                <button className='shadow-sm rounded-pill'><BsFillCartFill />Add to cart</button>
+
+            </div>
+
+        </div>
+        <div className=' container product-info'>
+            <div className='product-name'>
+                <h3>{card_info.productName}</h3>
+
+            </div>
+            <div className='product-price'>
+                $<span>{card_info.productPrice}</span>
+
+            </div>
+            <div className='product-description'>
+                {card_info.ProductDescription}
+                <br />
+                <button> Read More...</button>
+
+            </div>
+
+
+        </div>
+    </div>
+
+</div>
+
+
+
+
+                    )
+                })
+            }
+            </div>
+           
+          
+
 
 
         </div>
---
+++ src/components/custom_card/Custom_card_data.js
@@ -0,0 +1,60 @@
+export const getdata = () =>
+[
+    {
+        "sale":"10%",
+        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2017/06/variable-product-example-2.jpg?fit=430%2C491&ssl=1",
+        "productName":"Box",
+        "productPrice":"30",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"10%",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/eames-lounge-chair-1.jpg",
+        "productName":"Box",
+        "productPrice":"30",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"10%",
+        "photo":"https://hishabkitab.com/wp-content/uploads/2017/06/eames-plastic-side-chair-3.jpg",
+        "productName":"Box",
+        "productPrice":"30",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"10%",
+        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
+        "productName":"Box",
+        "productPrice":"30",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"10%",
+        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
+        "productName":"Box",
+        "productPrice":"30",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"10%",
+        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
+        "productName":"Box",
+        "productPrice":"30",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"10%",
+        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
+        "productName":"Box",
+        "productPrice":"30",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    },
+    {
+        "sale":"10%",
+        "photo":"https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1",
+        "productName":"Box",
+        "productPrice":"30",
+        "ProductDescription":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia aliquid, excepturi dolores porro aut modi enim eius."
+    }
+  
+]
