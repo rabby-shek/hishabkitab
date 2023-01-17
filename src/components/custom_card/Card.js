909152a5efa0c60596ee3bde420dbb9fd791f248
rabby-shek <rabbyshek1998@gmail.com>
2023-01-17 10:40:04 +0600

    added

--- src/components/custom_card/Card.js
+++ src/components/custom_card/Card.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import './card.css'
+import { AiFillEye } from 'react-icons/ai';
+import { FiHeart } from 'react-icons/fi';
 import { MdCompareArrows } from 'react-icons/md';
+import { BsFillCartFill } from 'react-icons/bs';
+
+
 
 const Card = () => {
+    console.log('shop');
     return (
         <div className='custom_card'>
-            <div className='custom_card_box'>
-                <div className='card_image'>
-                    <img src='https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/ornare-auctor-1.jpg?fit=700%2C800&ssl=1' />
+            <div className='container'>
+                <div className='row'>
 
-                </div>
-                <div>
-                    compare
+                <h2>Featured Products</h2>
+                    <div className='col-md-3'>
+                        <div className='product-box'></div>
+                        <div className='product-inner-box position-relative'>
+                            <div className='icons position-absolute'>
+                                <a href='#' className='text-decoration-none text-dark'><FiHeart /></a>
+                                <a href='#' className='text-decoration-none text-dark'><AiFillEye /></a>
+                                <a href='#' className='text-decoration-none text-dark'><MdCompareArrows /></a>
+
+                            </div>
+                            <img src='https://i0.wp.com/hishabkitab.com/wp-content/uploads/2021/07/senectus-tincidunt-1.jpg?fit=430%2C491&ssl=1' alt='light' className='img-fluid'/>
+                            <div className='cart-btn'>
+                                <button className='btn bg-white shadow-sm rounded-pil'><BsFillCartFill />Add to Cart</button>
+
+                            </div>
+
+                        </div>
+                        <div className='product-info'>
+                            <div className='product-name'>
+                                <h3>Light</h3>
+                            </div>
+                            <div className='product-price'>
+                                $<span>179</span>
+
+                            </div>
+
+                        </div>
+
                     </div>
 
 
-
-                <h3>card title</h3>
-                <div className="custom_card_body">
-                    the name of our country is bangladesh. there are many problem in our country.
-                    wee need try to solve them.
-
                 </div>
-
 
             </div>
 
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -5,13 +5,11 @@ import Foter from '../foteer/Foter'
 import Card from '../custom_card/Card'
 
 const Shop = () => {
+  console.log("shop");
   return (
     <div> 
     <Navbar />
     <Card />
-   
-
-
     <Foter />
     </div>
   )
